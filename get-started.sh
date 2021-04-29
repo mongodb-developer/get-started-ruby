08737bd70bbe89764c6cb7f5fbd09591b21d43a9
Wan Bachtiar <sindbach@gmail.com>
2021-04-29 15:58:24 +1000

    Initial commit of get-started

---
+++ get-started.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+MONGODB_URI=${1}
+if [ -z ${MONGODB_URI} ]
+then
+    read -p "MONGODB URI (Required): " MONGODB_URI
+fi 
+
+echo "Executing ... "
+docker run --rm -e MONGODB_URI=${MONGODB_URI} \
+    -v "$(pwd)":/workspace/ruby \
+    -w /workspace/ruby ghcr.io/mongodb-developer/get-started-ruby:0.1 \
+    "ruby getstarted.rb"
--- getstarted.rb
+++ getstarted.rb
@@ -3,13 +3,16 @@ require 'json'
 
 mongoURI = ENV['MONGODB_URI']
 
-mongoClient = Mongo::Client.new(mongoURI)
-database = mongoClient.database
-collection = mongoClient[:tour]
 
+puts "Connecting to MongoDB"
+mongoClient = Mongo::Client.new(mongoURI, :database => 'getstarted')
+collection = mongoClient[:ruby]
+
+puts "Dropping collection 'getstarted.ruby' (command)"
 collection.drop
 
 # make a document and insert it
+puts "Inserting a single document"
 document = { name: "MongoDB", type: "database", count: 1, info: {x: 203, y: 102}}
 collection.insert_one(document)
 
@@ -17,41 +20,44 @@ collection.insert_one(document)
 documents = Array.new
 
 for i in 1..10
-	document = {i: i}
+	document = {type:"database", info: {x:i}}
 	documents.push document
 end
 
+puts "Inserting many documents"
 collection.insert_many(documents)
 
 # lets get all the documents in the collection and print them out
+puts "Querying using find()"
 collection.find.each do |document|
 	puts JSON.pretty_generate(document)
 end
 
+puts "Query documents with a predicate"
 # now use a query to get 1 document out
 myDocument = collection.find(:i => 3).first
 puts JSON.pretty_generate(myDocument)
 
 # Sorting
+puts "Query a document with a sort()"
 myDocument = collection.find().sort(:i => -1).first
 puts JSON.pretty_generate(myDocument)
 
 # Projection
+puts "Query a document and use projection"
 myDocument = collection.find({},{'projection' => {'_id' => 0 }}).first
 puts JSON.pretty_generate(myDocument)
 
 # aggregation
-database = mongoClient.use('taxidata')
-collection = database[:chicago]
 aggregation = collection.aggregate([
-	{'$match'=> {'trip_seconds' => {'$gte' => 1000}}},
-	{'$group'=> {'_id'=> '$payment_type', 'total_tips'=> {'$sum'=> $tips}, 'pickup_area'=> {'$addToSet'=> $pickup_community_area}}},
-	{'$project'=> {:payment_type=> '$_id', :total_tips=> '$total_tips', :pickup_area => '$pickup_area', :_id => 0}}
+	{'$group'=> {'_id'=> 'null', 'total'=> {'$sum'=> "$info.x"}}},
 ])
 
+puts "Aggregation result:"
 aggregation.each do |document|
 	puts JSON.pretty_generate(document)
 end
 
 # release resources
+puts "Finished."
 mongoClient.close
